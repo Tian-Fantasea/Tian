89bc71735613f7039ffd8f7b550bdea61ef2fb16
Tian <tt553093031@gmail.com>
2026-08-06 17:51:48 +0800

    add lz4
    Signed-off-by: Tian <tt553093031@gmail.com>

--- tests/lz4/lz4_test.sh
+++ tests/lz4/lz4_test.sh
@@ -159,9 +159,11 @@ phase1_build() {
 
     local os_id
     os_id="$(detect_os_id)"
+    local os_id_lower
+    os_id_lower="$(echo "${os_id}" | tr '[:upper:]' '[:lower:]')"
     log "PHASE1" "Building LZ4 from source on ${os_id}..."
 
-    case "${os_id}" in
+    case "${os_id_lower}" in
         ubuntu|debian)
             log "PHASE1" "Installing build dependencies (Ubuntu/Debian)..."
             sudo apt-get update -qq 2>&1 | tee -a "${LOG_FILE}"
